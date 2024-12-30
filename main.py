f6086da062c93543278ff0e29803368b3e803691
Congeladorportatil <41164425+Congeladorportatil@users.noreply.github.com>
2024-12-31 00:26:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def aumentar_volumen(input_file, output_file, factor):
     sf.write(output_file, data_aumentada, samplerate)
 
 nombre = input("Nombre del archivo:")
-archivo_entrada = f"{nombre}.wav"
-archivo_salida = f"{nombre}.wav"
 factor = input("Factor. \n Se recuerda que si es mayor que 1, se amplifica la señal; si es igual a 1 se mantiene la señal; y si es menor que 1, se reduce la amplitud.")
+archivo_entrada = f"{nombre}.wav"
+archivo_salida = f"{nombre}_amp" + f"{factor}.wav"
 
 aumentar_volumen(archivo_entrada, archivo_salida, factor)
